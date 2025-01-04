4ce53b902d1ac4b666082677839e5e9cc61cec36
ayushi-shahi <ayushishaho14072004@gmail.com>
2025-01-04 10:41:33 +0530

    Making code deployment ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,12 +7,13 @@ import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { app , server } from './lib/socket.js';
+import path from 'path'
 
 dotenv.config();
 
 const PORT = process.env.PORT
+const __dirname = path.resolve()
 
-// Middleware
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
@@ -23,11 +24,15 @@ app.use(
   })
 );
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname , "../frontend/dist")))
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
-// Start server
 server.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
   connectDB();
